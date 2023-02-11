76b2e631f0c1e7e2203637d2c688d47b9cffe846
kiang <kiange@gmail.com>
2023-02-11 21:28:52 +0800

    get area list

--- 02_kml2json.php
+++ 02_kml2json.php
@@ -8,6 +8,56 @@ $layers = [
     '電業籌設' => ['GHB002003', 'GHB011003', 'GHB011006'],
     '能源局列管案場' => ['鄉鎮市區', '段小段', '地號', '面積', '案件申請年', '業者名稱', '電廠名稱'],
 ];
+
+
+/*
+area functions from https://github.com/spinen/laravel-geometry/blob/develop/src/Support/GeometryProxy.php
+*/
+function determineCoordinateIndices($index, $length)
+{
+    // i = N-2
+    if ($index === ($length - 2)) {
+        return [$length - 2, $length - 1, 0];
+    }
+
+    // i = N-1
+    if ($index === ($length - 1)) {
+        return [$length - 1, 0, 1];
+    }
+
+    // i = 0 to N-3
+    return [$index, $index + 1, $index + 2];
+}
+
+function radians($degrees)
+{
+    return $degrees * M_PI / 180;
+}
+
+function ringArea($coordinates)
+{
+    $area = 0.0;
+
+    $length = count($coordinates);
+
+    if ($length <= 2) {
+        return $area;
+    }
+
+    for ($i = 0; $i < $length; $i++) {
+        list($lower_index, $middle_index, $upper_index) = determineCoordinateIndices($i, $length);
+
+        $point1 = $coordinates[$lower_index];
+        $point2 = $coordinates[$middle_index];
+        $point3 = $coordinates[$upper_index];
+
+        $area += (radians($point3[0]) - radians($point1[0])) * sin(radians($point2[1]));
+    }
+
+    return $area * 6378137 * 6378137 / 2;
+}
+
+$area = [];
 foreach ($layers as $layer => $keys) {
     $kml = simplexml_load_file(__DIR__ . '/kml/' . $layer . '.kml');
     $json = json_decode(json_encode($kml), true);
@@ -68,7 +118,22 @@ foreach ($layers as $layer => $keys) {
             $f['geometry']['type'] = 'Polygon';
             $f['geometry']['coordinates'] = [$pool];
         }
+        if (!isset($area[$f['properties']['業者名稱']])) {
+            $area[$f['properties']['業者名稱']] = 0.0;
+        }
+        $areaSize = 0;
+        foreach ($f['geometry']['coordinates'] as $c) {
+            $areaSize += round(abs(ringArea($c)), 0);
+        }
+        $area[$f['properties']['業者名稱']] += $areaSize;
+
         $fc['features'][] = $f;
+    }
+
+    $oFh = fopen(__DIR__ . '/csv/' . $layer . '.csv', 'w');
+    fputcsv($oFh, ['業者名稱', '面積(公頃)']);
+    foreach ($area as $k => $v) {
+        fputcsv($oFh, [$k, round($v / 10000, 2)]);
     }
     file_put_contents(__DIR__ . '/json/' . $layer . '.json', json_encode($fc, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 }
